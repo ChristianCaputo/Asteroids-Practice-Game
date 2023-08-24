32a3956222ca21cf530269d7158c9d3cead228c4
ChristianCaputo <christiancaputo86@gmail.com>
2023-08-24 00:16:30 -0300

    Updated Player Movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 0;
+    private float turnDirection;
+    private Rigidbody2D rigidBody;
+    private bool moving;
+    private bool reverse;
+
     void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        moving = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // deteccion de inputs
+        moving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        reverse = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            turnDirection = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            turnDirection = -1.0f;
+        }
+        else
+        {
+            turnDirection = 0;
+        }
     }
 
-    void FixedUpdate(){}
+    void FixedUpdate()
+    {
+        // funciones addForce (para avanzar) y Torque (para girar)
+        if (moving)
+        {
+            speed = 4;
+            rigidBody.AddForce(this.transform.up * speed);
+        }
+        if(reverse)
+        {
+            //rigidBody.AddForce(this.transform.up * -speed);
+            speed = 0;
+        }
+        if (turnDirection != 0)
+        {
+            rigidBody.AddTorque(turnDirection);
+        }
+    }
 }
